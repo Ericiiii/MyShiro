dd54942c20883972395ffdc7516554fae4aaed7e
Ericiiii <iloveyouqingqing@gmail.com>
2017-08-15 14:52:05 +0800

    commit

--- src/main/java/com/mayday/Application.java
+++ src/main/java/com/mayday/Application.java
@@ -50,7 +50,6 @@ public class Application {
 
     public static void main(String [] args){
         SpringApplication.run(Application.class,args);
-        System.out.println("mayday111");
 
     }
 
